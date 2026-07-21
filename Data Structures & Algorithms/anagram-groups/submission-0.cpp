ab9cd88a9e902122af50dde3f2213b98ab0a60c1
Manasi Saiba <manasisaiba99@gmail.com>
2026-07-21 22:40:04 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& str) {
+        int n = str.size();
+        vector<vector<string>>res;
+        unordered_map<string,vector<string>> mp;
+        for(int i = 0;i<n;i++){
+         string s = str[i];
+         sort(s.begin(), s.end());
+         mp[s].push_back(str[i]);
+        }
+        for(auto i:mp){
+         res.push_back(i.second);
+        }
+        return res;
+    }
+};
