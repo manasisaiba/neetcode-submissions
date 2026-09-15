7433bebcf6d64cd62158d0f87fdc3f6baf5477cc
Manasi Saiba <manasisaiba99@gmail.com>
2026-09-16 00:04:24 +0530

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> start, end;
+        for(auto i: intervals){
+            start.push_back(i.start);
+            end.push_back(i.end);
+        }
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
+
+        int s=0, e=0, count=0,mini=0;
+        while(s<intervals.size()){
+            if(start[s]<end[e]){
+                s++;
+                count++;
+            }else{
+                e++;
+                count--;
+            }
+            mini=max(mini,count);
+        }
+        return mini;
+    }
+};
